07a821e793b7922873e53764e96bd3c1bc9a1690
Leandro Gustavo Neis <lgn200@gmail.com>
2021-11-08 21:58:54 -0300

    Inserção de usuário com POST

--- src/main/java/com/leandroneis/workshopmongo/services/UserService.java
+++ src/main/java/com/leandroneis/workshopmongo/services/UserService.java
@@ -3,6 +3,7 @@ package com.leandroneis.workshopmongo.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.leandroneis.workshopmongo.dto.UserDTO;
 import com.leandroneis.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj){
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto){
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
 	}
 }
