8a9fe7f384bcea56511df3d9e7b403bcf0e030e7
Leandro Gustavo Neis <lgn200@gmail.com>
2021-11-10 13:01:00 -0300

    Atualização de usuário com PUT

--- src/main/java/com/leandroneis/workshopmongo/resources/UserResource.java
+++ src/main/java/com/leandroneis/workshopmongo/resources/UserResource.java
@@ -47,4 +47,13 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
+	//Outra alternativa @PostMapping
+	@RequestMapping(value="/{id}",method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto,@PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
