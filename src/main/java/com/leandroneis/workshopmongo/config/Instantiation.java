a9092cd07b3d5cbfdb874e398282f9fd6b6ee85e
Leandro Gustavo Neis <lgn200@gmail.com>
2021-11-12 01:00:06 -0300

    Referenciando os posts do usuário

--- src/main/java/com/leandroneis/workshopmongo/config/Instantiation.java
+++ src/main/java/com/leandroneis/workshopmongo/config/Instantiation.java
@@ -41,6 +41,10 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia!","Acordei Feliz Hoje!",new AuthorDTO(maria));
 
 		postReposiroty.saveAll(Arrays.asList(post1,post2));
+
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+
+		userReposiroty.save(maria);
 	}
 
 }
