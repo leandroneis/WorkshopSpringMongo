5c46baf4a9eb9ff6fdfaea1a98ebe120bac1b981
Leandro Gustavo Neis <lgn200@gmail.com>
2021-11-18 08:15:51 -0300

    Consulta simples com @Query

--- src/main/java/com/leandroneis/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leandroneis/workshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.leandroneis.workshopmongo.repository;
 
 import com.leandroneis.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,8 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+    @Query("{ title: { $regex: ?0, $options: 'i'} }")
+    List<Post> searchTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
-
-
 }
